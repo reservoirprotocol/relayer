98e35fc882d3e6323e84228502cd475ec331c89b
George Roman <george.roman.99@gmail.com>
2022-01-19 11:45:51 +0100

    Tweaks

--- src/utils/sync-collection.ts
+++ src/utils/sync-collection.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import { db, pgp } from "../common/db";
 import { logger } from "../common/logger";
+import { config } from "../config";
 import { buildFetchAssetsURL, parseOpenseaOrder } from "./opensea";
 
 export const syncCollection = async (collection: string) => {
@@ -21,96 +22,97 @@ export const syncCollection = async (collection: string) => {
       limit,
     });
 
-    await axios.get(url).then(async (response: any) => {
-      const validOrders: Order[] = [];
-      const insertQueries: any[] = [];
-
-      const assets = response.data.assets;
-      for (const asset of assets) {
-        if (asset.sell_orders) {
-          for (const order of asset.sell_orders) {
-            const parsed = parseOpenseaOrder(order);
-            if (parsed) {
-              validOrders.push(parsed);
+    await axios
+      .get(
+        url,
+        config.chainId === 1
+          ? {
+              headers: {
+                "x-api-key": config.backfillOpenseaApiKey,
+                // https://twitter.com/lefterisjp/status/1483222328595165187?s=21
+                "user-agent":
+                  "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+              },
+              timeout: 5000,
             }
+          : // Skip including the API key on Rinkeby or else the request will fail
+            { timeout: 5000 }
+      )
+      .then(async (response: any) => {
+        const validOrders: Order[] = [];
+        const insertQueries: any[] = [];
 
-            // Skip saving any irrelevant information
-            delete (order as any).asset;
+        const assets = response.data.assets;
+        for (const asset of assets) {
+          if (asset.sell_orders) {
+            for (const order of asset.sell_orders) {
+              const parsed = parseOpenseaOrder(order);
+              if (parsed) {
+                validOrders.push(parsed);
+              }
 
-            // TODO: Use multi-row inserts for better performance
-            insertQueries.push({
-              query: `
-                insert into "orders"(
-                  "hash",
-                  "target",
-                  "maker",
-                  "created_at",
-                  "data"
-                )
-                values ($1, $2, $3, $4, $5)
-                on conflict do nothing
-              `,
-              values: [
-                order.prefixed_hash,
-                order.target,
-                order.maker.address,
-                Math.floor(new Date(order.created_date).getTime() / 1000),
-                order as any,
-              ],
-            });
+              // Skip saving any irrelevant information
+              delete (order as any).asset;
+
+              // TODO: Use multi-row inserts for better performance
+              insertQueries.push({
+                query: `
+                  insert into "orders"(
+                    "hash",
+                    "target",
+                    "maker",
+                    "created_at",
+                    "data"
+                  )
+                  values ($1, $2, $3, $4, $5)
+                  on conflict do nothing
+                `,
+                values: [
+                  order.prefixed_hash,
+                  order.target,
+                  order.maker.address,
+                  Math.floor(new Date(order.created_date).getTime() / 1000),
+                  order as any,
+                ],
+              });
+            }
           }
         }
-      }
 
-      if (insertQueries.length) {
-        await db.none(pgp.helpers.concat(insertQueries));
-      }
+        if (insertQueries.length) {
+          await db.none(pgp.helpers.concat(insertQueries));
+        }
 
-      // Post orders to Indexer V2
-      // TODO: Remove once Indexer V2 gets deprecated
-      if (process.env.BASE_INDEXER_V2_API_URL) {
-        await axios
-          .post(`${process.env.BASE_INDEXER_V2_API_URL}/orders/wyvern-v2`, {
-            orders: validOrders,
-          })
-          .catch((error) => {
-            logger.error(
-              "sync_collection",
-              `(${collection}) Failed to post orders to Indexer V2: ${error}`
-            );
-          });
-      }
+        // Post orders to Indexer V3
+        if (process.env.BASE_INDEXER_V3_API_URL) {
+          await axios
+            .post(`${process.env.BASE_INDEXER_V3_API_URL}/orders`, {
+              orders: validOrders.map((data) => ({
+                kind: "wyvern-v2",
+                data,
+              })),
+            })
+            .catch((error) => {
+              logger.error(
+                "sync_collection",
+                `(${collection}) Failed to post orders to Indexer V3: ${error}`
+              );
+            });
+        }
 
-      // Post orders to Indexer V3
-      if (process.env.BASE_INDEXER_V3_API_URL) {
-        await axios
-          .post(`${process.env.BASE_INDEXER_V3_API_URL}/orders`, {
-            orders: validOrders.map((data) => ({
-              kind: "wyvern-v2",
-              data,
-            })),
-          })
-          .catch((error) => {
-            logger.error(
-              "sync_collection",
-              `(${collection}) Failed to post orders to Indexer V3: ${error}`
-            );
-          });
-      }
+        numOrders += validOrders.length;
 
-      numOrders += validOrders.length;
+        if (assets.length < limit) {
+          done = true;
+        } else {
+          offset += limit;
+        }
 
-      if (assets.length < limit) {
-        done = true;
-      } else {
-        offset += limit;
-      }
-
-      // OpenSea doesn't allow offset greater than 10.000
-      if (offset >= 10000) {
-        done = true;
-      }
-    });
+        // OpenSea doesn't allow offset greater than 10.000
+        if (offset >= 10000) {
+          done = true;
+        }
+      });
   }
 
   logger.info("sync_collection", `(${collection}) Got ${numOrders} orders`);
