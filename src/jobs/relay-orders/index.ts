f91cf1a3fb5d40fbe706224aff7c4a31c748de7e
George Roman <george.roman.99@gmail.com>
2022-02-24 20:13:12 +0200

    Debug log

--- src/jobs/relay-orders/index.ts
+++ src/jobs/relay-orders/index.ts
@@ -71,6 +71,7 @@ const worker = new Worker(
               { orders },
               { timeout: 60000 }
             )
+            .then((response) => console.log(response.data))
             .catch((error) => {
               logger.error(
                 "relay_orders",
