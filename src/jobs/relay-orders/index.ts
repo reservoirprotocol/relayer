49f04ba0907d6184c209e308d4d288ee8a1465cf
George Roman <george.roman.99@gmail.com>
2022-02-28 16:31:54 +0200

    Fix

--- src/jobs/relay-orders/index.ts
+++ src/jobs/relay-orders/index.ts
@@ -3,6 +3,7 @@ import { Job, Queue, QueueScheduler, Worker } from "bullmq";
 
 import { logger } from "../../common/logger";
 import { redis } from "../../common/redis";
+import { config } from "../../config";
 
 const QUEUE_NAME = "relay-orders";
 
@@ -34,59 +35,61 @@ export const addToRelayOrdersQueue = async (
   );
 };
 
-// const worker = new Worker(
-//   QUEUE_NAME,
-//   async (job: Job) => {
-//     const { orders } = job.data;
+if (config.doBackgroundWork) {
+  const worker = new Worker(
+    QUEUE_NAME,
+    async (job: Job) => {
+      const { orders } = job.data;
 
-//     if (orders.length) {
-//       logger.info("relay_orders", `Relaying ${orders.length} orders`);
+      if (orders.length) {
+        logger.info("relay_orders", `Relaying ${orders.length} orders`);
 
-//       const requests: Promise<any>[] = [];
+        const requests: Promise<any>[] = [];
 
-//       // Post orders to Indexer V3
-//       if (process.env.BASE_INDEXER_V3_API_URL) {
-//         requests.push(
-//           axios
-//             .post(
-//               `${process.env.BASE_INDEXER_V3_API_URL}/orders`,
-//               { orders },
-//               { timeout: 3 * 60000 }
-//             )
-//             .catch((error) => {
-//               logger.error(
-//                 "relay_orders",
-//                 `Failed to relay orders to Indexer V3: ${error}`
-//               );
-//               throw error;
-//             })
-//         );
-//       }
+        // Post orders to Indexer V3
+        if (process.env.BASE_INDEXER_V3_API_URL) {
+          requests.push(
+            axios
+              .post(
+                `${process.env.BASE_INDEXER_V3_API_URL}/orders`,
+                { orders },
+                { timeout: 3 * 60000 }
+              )
+              .catch((error) => {
+                logger.error(
+                  "relay_orders",
+                  `Failed to relay orders to Indexer V3: ${error}`
+                );
+                throw error;
+              })
+          );
+        }
 
-//       // Post orders to Indexer Lite
-//       if (process.env.BASE_INDEXER_LITE_API_URL) {
-//         requests.push(
-//           axios
-//             .post(
-//               `${process.env.BASE_INDEXER_LITE_API_URL}/orders/v1`,
-//               { orders },
-//               { timeout: 60000 }
-//             )
-//             .catch((error) => {
-//               logger.error(
-//                 "relay_orders",
-//                 `Failed to relay orders to Indexer Lite: ${error}`
-//               );
-//               throw error;
-//             })
-//         );
-//       }
+        // Post orders to Indexer Lite
+        if (process.env.BASE_INDEXER_LITE_API_URL) {
+          requests.push(
+            axios
+              .post(
+                `${process.env.BASE_INDEXER_LITE_API_URL}/orders/v1`,
+                { orders },
+                { timeout: 60000 }
+              )
+              .catch((error) => {
+                logger.error(
+                  "relay_orders",
+                  `Failed to relay orders to Indexer Lite: ${error}`
+                );
+                throw error;
+              })
+          );
+        }
 
-//       await Promise.all(requests);
-//     }
-//   },
-//   { connection: redis.duplicate(), concurrency: 5 }
-// );
-// worker.on("error", (error) => {
-//   logger.error(QUEUE_NAME, `Worker errored: ${error}`);
-// });
+        await Promise.all(requests);
+      }
+    },
+    { connection: redis.duplicate(), concurrency: 5 }
+  );
+  worker.on("error", (error) => {
+    logger.error(QUEUE_NAME, `Worker errored: ${error}`);
+  });
+}
