7a747bb41d45a794e27a0ff48aaa6af8d9962eac
Idan Peleg <ipeleg@hotmail.com>
2022-03-24 14:53:11 +0000

    feat: sync realtime queue by last sync event timestamp

--- src/jobs/opensea-sync/index.ts
+++ src/jobs/opensea-sync/index.ts
@@ -1,47 +1,35 @@
 import cron from "node-cron";
 
-import { acquireLock, redis } from "../../common/redis";
-import { config } from "../../config";
-
-import * as openseaSyncRealtime from "./realtime-queue";
-import * as openseaSyncBackfill from "./backfill-queue";
-
 import "./realtime-queue";
 import "./backfill-queue";
 
-if (config.doBackgroundWork) {
+import { acquireLock, redis } from "../../common/redis";
+import { config } from "../../config";
+import { logger } from "../../common/logger";
+import { realtimeQueue } from "./realtime-queue";
+
+import * as openseaSyncRealtime from "./realtime-queue";
+
+if (true) {
   // Fetch new orders every 1 minute
   cron.schedule("*/1 * * * *", async () => {
-    const currentMinute = Math.floor(Date.now() / 1000 / 60);
-    const previousMinute = currentMinute - 1;
-    const numberOfJobs = 12;
-    const interval = 5;
+    const currentTimestamp = Math.floor(Date.now() / 1000 / 60);
 
-    const lockAcquired = await acquireLock("opensea-sync-lock", 55);
+    const lockAcquired = await acquireLock("opensea-sync-lock", 60);
 
     if (lockAcquired) {
-      const cacheKey = "opensea-sync-last-minute";
-      const lastSyncedMinute = Number(await redis.get(cacheKey));
+      const cacheKey = "opensea-sync-last-second";
+      let lastSyncedSecond = Number(await redis.get(cacheKey));
 
-      if (lastSyncedMinute === 0) {
+      if (lastSyncedSecond === 0) {
         // No cache, so we only sync the last minute
-        await openseaSyncBackfill.addToBackfillQueue(previousMinute, previousMinute);
-      } else if (lastSyncedMinute < previousMinute) {
-        // Create a 5s interval job to sync orders
-        for (let i = 0; i < numberOfJobs; i++) {
-          const second = currentMinute * 60 + interval * i;
-          const delayMs = interval * (i + 1) * 1000;
-
-          await openseaSyncRealtime.addToRealtimeQueue(second, interval, delayMs);
-        }
-
-        // If we need to do any backfill e.g. last sync is older than 1 minuted ago
-        if (lastSyncedMinute < previousMinute - 1) {
-          await openseaSyncBackfill.addToBackfillQueue(lastSyncedMinute, previousMinute - 1);
-        }
+        lastSyncedSecond = currentTimestamp - 60;
+        await redis.set(cacheKey, lastSyncedSecond);
       }
 
-      await redis.set(cacheKey, String(previousMinute));
+      await openseaSyncRealtime.addToRealtimeQueue();
+
+      logger.info(realtimeQueue.name, `Start sync from lastSyncedSecond=(${lastSyncedSecond})`);
     }
   });
 }
