a3ec515d35333b8acf42b55c7c7fa445c27d7b20
George Roman <george.roman.99@gmail.com>
2022-04-20 00:10:58 +0300

    fix: tweak

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,15 @@ import "./common/tracer";
 import "./jobs";
 
 import { start } from "./api";
+import { backfillQueue } from "./jobs/opensea-sync/backfill-queue";
+
+const main = async () => {
+  try {
+    backfillQueue.clean(0, 100000, "wait");
+    backfillQueue.clean(0, 100000, "delayed");
+  } catch {}
+};
+
+main();
 
 start();
