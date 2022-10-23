66b804bb7ba54acf9731f35b6fcf50a135e49878
Idan Peleg <ipeleg@hotmail.com>
2022-10-22 22:51:09 -0400

    feat: turn off bids with env param

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,7 @@ export const config = {
   doBackfillWork: Boolean(Number(process.env.DO_BACKFILL_WORK)),
   doRealtimeWork: Boolean(Number(process.env.DO_REALTIME_WORK)),
   doLiveWork: Boolean(Number(process.env.DO_LIVE_WORK)),
+  doOffers: Boolean(Number(process.env.DO_OFFERS)),
 
   databaseUrl: String(process.env.DATABASE_URL),
   redisUrl: String(process.env.REDIS_URL),
