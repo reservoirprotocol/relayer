a2741869d33f62c28a042df27b398b27833bddd6
vikinatora <vikinatora@gmail.com>
2022-11-09 12:06:57 +0200

    add rarible to source type

--- src/migrations/1666614255718_my-add-rarible-source.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
-
-export const shorthands: ColumnDefinitions | undefined = undefined;
-
-export async function up(pgm: MigrationBuilder): Promise<void> {}
