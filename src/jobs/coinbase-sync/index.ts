8ddae0cdd029cdf977c963d4424b7cdc8cebbd4b
Idan Peleg <ipeleg@hotmail.com>
2022-11-14 21:11:28 -0500

    feat: sync coinbase bids

--- src/jobs/coinbase-sync/index.ts
+++ src/jobs/coinbase-sync/index.ts
@@ -4,6 +4,7 @@ import _ from "lodash";
 import { config } from "../../config";
 import { acquireLock } from "../../common/redis";
 import * as coinbaseSyncRealtime from "./realtime-queue";
+import * as coinbaseOffersSyncRealtime from "./realtime-queue-offers";
 
 if (config.doRealtimeWork) {
   cron.schedule("*/10 * * * * *", async () => {
@@ -12,6 +13,14 @@ if (config.doRealtimeWork) {
 
       if (lockAcquired) {
         await coinbaseSyncRealtime.addToRealtimeQueue();
+      }
+    }
+
+    if (_.indexOf([1], config.chainId) !== -1) {
+      const lockAcquired = await acquireLock("coinbase-offers-sync-lock", 120);
+
+      if (lockAcquired) {
+        await coinbaseOffersSyncRealtime.addToRealtimeQueue();
       }
     }
   });
