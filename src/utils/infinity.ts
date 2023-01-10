f75fcb0ffa997e8219aa16972d9e9c719271fbc5
Idan Peleg <ipeleg@hotmail.com>
2023-01-10 12:58:40 -0500

    feat: log full infinity order

--- src/utils/infinity.ts
+++ src/utils/infinity.ts
@@ -72,7 +72,7 @@ export class Infinity {
     try {
       return new Sdk.Infinity.Order(config.chainId, order.signedOrder);
     } catch (err) {
-      logger.error("parse-infinity-order", `Failed to parse order ${order} - ${err}`);
+      logger.error("parse-infinity-order", `Failed to parse order ${JSON.stringify(order)} - ${err}`);
     }
   }
 }
