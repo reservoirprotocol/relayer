4e7695a9478c8827d5d5e7f544752cda122c04a7
George Roman <george.roman.99@gmail.com>
2023-04-06 15:55:04 +0200

    fix: debug

--- src/jobs/seaport-sync/utils.ts
+++ src/jobs/seaport-sync/utils.ts
@@ -149,8 +149,6 @@ export const fetchOrders = async (side: "sell" | "buy", apiKey = "", overrideBas
 
       throw error;
     }
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 
   logger.info("fetch_orders_seaport", `Seaport - Done. side=${side}, total=${total}`);
