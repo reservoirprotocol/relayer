083e82b4c73776ed48c4f7dcc99520584f881062
Idan Peleg <ipeleg@hotmail.com>
2023-08-07 16:57:37 +0300

    feat: pass looksrare api key

--- src/jobs/looksrare-v2-sync/utils.ts
+++ src/jobs/looksrare-v2-sync/utils.ts
@@ -48,14 +48,19 @@ export const fetchOrders = async (
         config.chainId === 1
           ? {
               headers: {
-                // "X-Looks-Api-Key": config.looksrareApiKey,
+                "X-Looks-Api-Key": config.looksrareApiKey,
                 "user-agent":
                   "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
               },
               timeout: 10000,
             }
           : // Skip including the API key on Rinkeby or else the request will fail
-            { timeout: 10000 }
+            {
+              headers: {
+                "X-Looks-Api-Key": config.looksrareApiKey,
+              },
+              timeout: 10000,
+            }
       );
 
       const orders: LooksRareOrderV2[] = response.data.data;
