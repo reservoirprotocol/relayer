f5551e2bbfdd3b8ebe3ef7ff935bf145d337a056
George Roman <30772943+georgeroman@users.noreply.github.com>
2024-06-19 18:17:22 +0200

    feat: update sdk

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
+import * as Sdk from "@reservoir0x/sdk";
+
+// Initialize the SDK - this step must be done before reaching any imports that reference the SDK
+Sdk.Global.Config.addresses = Sdk.Addresses;
+
 import "./common/tracer";
 import "./jobs";
 
