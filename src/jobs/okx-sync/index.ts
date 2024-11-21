9cd4322d9c2eb0cc7a99a6c3abe9f5f2395ac52c
nofir <ofirsud@gmail.com>
2024-11-21 13:21:48 -0500

    feat: add env var

--- src/jobs/okx-sync/index.ts
+++ src/jobs/okx-sync/index.ts
@@ -5,13 +5,14 @@ import { acquireLock } from "../../common/redis";
 import { config } from "../../config";
 import { Okx } from "../../utils/okx";
 
-if (config.doRealtimeWork) {
+if (config.doRealtimeWork && config.doOkxWork) {
   if (new Okx().getChainName()) {
     cron.schedule("*/30 * * * * *", async () => {
       const lockAcquired = await acquireLock(
         realtimeQueueListings.getLockKey(),
         60 * 10
       );
+
       if (lockAcquired) {
         await realtimeQueueListings.addToRealtimeQueue();
       }
